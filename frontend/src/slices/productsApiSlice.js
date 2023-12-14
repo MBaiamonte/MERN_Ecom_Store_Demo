d4fa00c3459f56be3f718b0e6ac2b5475900175e
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2023-12-13 17:49:43 -0700

    set up fetching for get one product through redux toolkit

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -9,9 +9,15 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 url:PRODUCTS_URL  
             }),
             keepUnusedDataFor: 5
-        })
+        }), //end getAll
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url:`${PRODUCTS_URL}/${productId}` 
+            }),
+            keepUnusedDataFor: 5
+        }) //end getOne
     }),
 });
 
 
-export const {useGetProductsQuery} = productsApiSlice;
+export const {useGetProductsQuery,useGetProductDetailsQuery} = productsApiSlice;
--- frontend/src/views/ProductScreen.jsx
+++ frontend/src/views/ProductScreen.jsx
@@ -1,72 +1,83 @@
-import React from 'react'
-import {useState,useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import {Row,Col,Image,ListGroup,Card,Button} from 'react-bootstrap'
-import Rating from '../components/Rating'
-import axios from 'axios';
+import React from 'react';
+import {useParams} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {Row,Col,Image,ListGroup,Card,Button} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import {useGetProductDetailsQuery} from '../slices/productsApiSlice';
+// import {useState,useEffect} from 'react';
+// import axios from 'axios';
 
 const ProductScreen = () => {
-    const [product,setProduct] = useState({});
     const {id: productId} = useParams();
+    const {data:product,isLoading,error} = useGetProductDetailsQuery(productId);
 
-    useEffect(()=>{
-        const fetchProduct = async ()=>{
-            const {data}= await axios.get(`/api/products/${productId}`);
-            setProduct(data)
-        }
-        fetchProduct();
-    },[productId]) //end use effect
+
+    // const [product,setProduct] = useState({});
+    // useEffect(()=>{
+    //     const fetchProduct = async ()=>{
+    //         const {data}= await axios.get(`/api/products/${productId}`);
+    //         setProduct(data)
+    //     }
+    //     fetchProduct();
+    // },[productId]) //end use effect
+//^^ using redux to manage state. use this if redux isn't being used.
 
     return (
     <>
         <Link className='btn btn-light my-3' to="/">HomePage</Link>
-        <Row>
-            <Col md={5}>
-                <Image src={product.image} alt={product.name} fluid />
-            </Col>
-            <Col md={4}>
-                <ListGroup variant='flush'>
-                    <ListGroup.Item>
-                        <h3>{product.name}</h3>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        Price: ${product.price}
-                    </ListGroup.Item>
-                    <ListGroup.Item>Description: {product.description}</ListGroup.Item>
-                </ListGroup>
-            </Col>
-            <Col md={3}>
-                <Card>
+
+        {isLoading? (
+        <h2>Loading...</h2>
+        ) : error ? (
+            <div>{error?.data?.message || error.error }</div>
+        ) : (
+                    <Row>
+                <Col md={5}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={4}>
                     <ListGroup variant='flush'>
                         <ListGroup.Item>
-                            <Row>
-                                <Col>Price:</Col>
-                                <Col>
-                                    <strong>${product.price}</strong>
-                                </Col>
-                            </Row>
+                            <h3>{product.name}</h3>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Row>
-                                <Col>Status:</Col>
-                                <Col>
-                                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</strong>
-                                </Col>
-                            </Row>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Button className='btn-block' type='button' disabled={product.countInStock ===0}>Add To Cart</Button>
+                            Price: ${product.price}
                         </ListGroup.Item>
+                        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
                     </ListGroup>
-                </Card>
-            </Col>
-        </Row>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        <strong>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button className='btn-block' type='button' disabled={product.countInStock ===0}>Add To Cart</Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        )}
     </>
-    )
-}
+    ) // end return
+} //end component
 
 export default ProductScreen
