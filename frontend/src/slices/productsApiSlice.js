10727c70314efac364288ac8ce6406fe97a80ec0
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-01-03 16:08:22 -0700

    enabled users to leave product reviews.

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -45,8 +45,16 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 method: 'DELETE',
             }),
         }),//end deleteProduct
+        createReview: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+                method: 'POST',
+                body: data,
+            }),
+            invalidatesTags: ['Product']
+        }), //end create review
     }),//end all endpoints
 });//end productApiSlice
 
 
-export const {useGetProductsQuery,useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation} = productsApiSlice;
+export const {useGetProductsQuery,useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation, useCreateReviewMutation} = productsApiSlice;
