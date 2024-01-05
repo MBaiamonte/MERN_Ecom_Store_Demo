a498105373d79f0a9a8fb12e9efe195aecc77a9b
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-01-05 14:47:52 -0700

    prepared  for deployment with render

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ app.use(express.urlencoded({extended:true}));
 //Cookie Parser Middleware (allows us to access req.cookies.jst )
 app.use(cookieParser());
 
-app.get('/', (req,res)=>{
-    res.send('API is running...')
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -35,6 +32,19 @@ app.get('/api/config/paypal', (req,res)=>res.send({clientId: process.env.PAYPAL_
 
 const __dirname= path.resolve(); //set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    // any route thats not api will be redirected to indexedDB.html
+    app.get('*', (req,res)=> 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+} else{
+    app.get('/', (req,res)=>{
+        res.send('API is running...')
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
