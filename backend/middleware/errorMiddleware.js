0f0559584bb8ac723ab556ddc90f9775c59bb368
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-01-06 16:15:18 -0700

    fixed bug with object  id error

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,11 +8,6 @@ const errorHandler = (err, req, res, next)=>{
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode; //changes error codes from 200 to 500. if not a 200 error it'll keep it as what ever it is. 
     let message = err.message;
 
-    // (below) checking for mongoose bad object id and have it give back a message instead of html doc
-    if (err.name === 'CastError' && err.kind === 'ObjectId'){
-        message = `Resource not found`;
-        statusCode = 404;
-    }
     res.status(statusCode).json({
         message: message,
         stack: process.env.NODE_ENV === 'production' ? null: err.stack,
