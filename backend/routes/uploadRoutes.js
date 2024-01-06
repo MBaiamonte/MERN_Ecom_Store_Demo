0074139ae8692b9020b9bb15a4ab01a46f0f20b3
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-01-06 16:01:25 -0700

    Fixed file upload bug and included file type verification middleware

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -13,26 +13,30 @@ const storage = multer.diskStorage({
     }
 });
 
-function checkFileType(file, cb){
-    const filetypes = /jph|jpeg|png/;
-    const extname= filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-    if(extname && mimetype){
-        return cb(null, true);
-    }else{
-        cb('Images Only!');
+function fileFilter(req, file, cb) {
+    const filetypes = /jpe?g|png|webp/;
+    const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = mimetypes.test(file.mimetype);
+    if (extname && mimetype) {
+        cb(null, true);
+    } else {
+        cb(new Error('Images only!'), false);
     }
-}
+};
 
-const upload=multer({
-    storage,
-});
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single('image');
 
-router.post('/', upload.single('image'), (req,res)=>{
-    res.send({
-        message: 'Image Uploaded',
-        image: `/${req.file.path}`
-
+router.post('/', (req, res) => {
+    uploadSingleImage(req, res, function (err) {
+        if (err) {
+            return res.status(400).send({ message: err.message });
+        }
+        res.status(200).send({
+        message: 'Image uploaded successfully',
+        image: `/${req.file.path}`,
+        });
     });
 });
 
